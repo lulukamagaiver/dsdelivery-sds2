db5f728c91a72f35a8fd8b1ff86f447215b8af13
Marcus Vinicius <luluka.magaiver@gmail.com>
2021-01-06 19:05:53 -0300

    Add Footer page

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,18 @@
+import './style.css'
+import {ReactComponent as InstagranIcon} from './instagram.svg';
+import {ReactComponent as LinkedInIcon} from './linkedin.svg';
+import {ReactComponent as YouTubeIcon} from './youtube.svg';
+function Footer() {
+    return (
+        <footer className="main-footer">
+            App desenvolvido na 2 Edição do evento Semana DevSuperior
+            <div className="footer-icons">
+                <a href="https://youtube.com" target="_blank"><YouTubeIcon/></a>
+                <a href="https://www.linkedin.com/in/marcus-vinícius-718a14189/" target="_blank"><LinkedInIcon/></a>
+                <a href="http://instagran.com" target="_blank" ><InstagranIcon/></a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
