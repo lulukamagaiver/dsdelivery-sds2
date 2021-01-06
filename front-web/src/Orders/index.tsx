e757b0aa6468b9a5e9c0b4274cd9d95dbef1bf7d
Marcus Vinicius <luluka.magaiver@gmail.com>
2021-01-06 20:56:50 -0300

    Add Product list and conect in api

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,9 +1,20 @@
+import { useEffect, useState } from 'react'
+import ProductList from './ProductList'
 import StepsHeader from './StepsHeader'
 import './style.css'
+import { Product } from './types';
+import { fetchProducts } from '../api';
 function Order() {
+    const [products, setProducts] = useState<Product[]>([]);   
+    useEffect(()=>{
+        fetchProducts()
+            .then(res => setProducts(res.data))
+            .catch(error => console.error(error));
+    }, [])
     return (
         <div className="orders-container">
             <StepsHeader/>
+            <ProductList products={products}/>
         </div>
     )
 }
