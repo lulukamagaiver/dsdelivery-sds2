464e3486cf4d500ef05f1219f85423f88e67bebe
Marcus Vinicius <luluka.magaiver@gmail.com>
2021-01-06 21:01:31 -0300

    Ajusts

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -7,9 +7,9 @@ function Footer() {
         <footer className="main-footer">
             App desenvolvido na 2 Edição do evento Semana DevSuperior
             <div className="footer-icons">
-                <a href="https://youtube.com" target="_blank"><YouTubeIcon/></a>
-                <a href="https://www.linkedin.com/in/marcus-vinícius-718a14189/" target="_blank"><LinkedInIcon/></a>
-                <a href="http://instagran.com" target="_blank" ><InstagranIcon/></a>
+                <a href="https://youtube.com" target="_blank" rel="noreferrer"><YouTubeIcon/></a>
+                <a href="https://www.linkedin.com/in/marcus-vinícius-718a14189/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+                <a href="http://instagran.com" target="_blank" rel="noreferrer"><InstagranIcon/></a>
             </div>
         </footer>
     )
